be7d1ab35fa79ea7e6b927ad5c501be893fe29c1
ilyasbabu <ilyasbabuvp@gmail.com>
2022-02-04 22:32:13 +0530

    postgress DB connected

--- qbankproject/settings.py
+++ qbankproject/settings.py
@@ -87,8 +87,11 @@ WSGI_APPLICATION = 'qbankproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'questionbank',
+        'USER' : 'postgres',
+        'PASSWORD': '1234',
+        'HOST':'localhost'
     }
 }
 
