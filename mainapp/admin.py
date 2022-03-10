4136ccc1bc2722e2c8db9154419f96ad8d5ca8ef
ilyasbabu <ilyasbabuvp@gmail.com>
2022-03-10 16:26:46 +0530

    Project Time zone changed, admin panel updated

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -4,12 +4,24 @@ from .models import *
 # Register your models here.
 admin.site.register(university)
 admin.site.register(department)
-admin.site.register(subject)
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('__str__','department')
+admin.site.register(subject,SubjectAdmin)
+
 class quesAdmin(admin.ModelAdmin):
     list_display=['__str__','subject','username','show']
     list_editable=['show']
+    list_filter=['subject','username']
+    search_fields=['ques','answer','subject__subject']
 admin.site.register(questionanswer,quesAdmin)
 
 class feedbackAdmin(admin.ModelAdmin):
     list_display=['__str__','name','email']
 admin.site.register(feedback_m,feedbackAdmin)
+
+
+
+admin.site.site_header = 'QUESTION BANK'
+admin.site.site_title = 'ADMIN PANEL'
+admin.site.index_title= 'Welcome to Question Bank Admin'
